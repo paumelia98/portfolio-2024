e6fbcf0ef4ca0ed47d3cce7faf7a6e58450211af
Pau <paumelia98@gmail.com>
2024-07-24 23:45:06 +0200

    Update portfolio

--- src/app/components/Layout/Header.jsx
+++ src/app/components/Layout/Header.jsx
@@ -28,7 +28,7 @@ export const Header = () => {
           muted
           playsInline
         ></video>
-        <div className="absolute top-0 left-0 w-full h-[50%] bg-[#f7f7f9] opacity-20 lg:opacity-0 z-0 rounded-b-xl"></div>
+        <div className="absolute top-0 left-0 w-full h-[50%] bg-[#f7f7f9] opacity-50 lg:opacity-0 z-0 rounded-b-xl"></div>
         <div className="relative z-10 flex flex-col lg:w-[900px] w-[400px] align-center">
           <h1 className="font-unbounded text-2xl lg:text-6xl text-center font-medium">Quality Frontend Development</h1>
           <h2 className="font-inter_tight text-lg lg:text-2xl text-center font-normal mt-4">I specialize in developing dynamic, user-friendly websites and apps that elevate user experience, optimize performance, and drive business growth.</h2>
