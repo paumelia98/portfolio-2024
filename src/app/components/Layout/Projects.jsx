86d2ee7f99c43fd43ef040ccbe322ef1dcc4f022
Pau <paumelia98@gmail.com>
2024-08-04 00:21:55 +0200

    Projects section

--- src/app/components/Layout/Projects.jsx
+++ src/app/components/Layout/Projects.jsx
@@ -3,23 +3,30 @@
 export const Projects = () => {
     return (
       <>
-      <div className="p-4 lg:px-12 mt-12 font-inter_tight grid grid-cols-4 gap-20">
+      <div className="p-4 lg:px-12 mt-12 font-inter_tight grid lg:grid-cols-4 grid-cols-1 gap-20">
       <div >
         <h2 className=" text-3xl font-medium">Projects</h2>
         <p className=" font-extralight mt-3">I have a proven track record of my own product and over 30 projects that have helped clients grow</p>
       </div>
-      <div className="col-span-3 grid grid-cols-2 gap-4 grid-rows-2  ">
+      <div className="lg:col-span-3 grid lg:grid-cols-2 gap-4 grid-rows-2  ">
         <article>
-        <img src="/laliga.png" className="rounded-lg" alt="" />
+        <img src="/laliga.png" className="rounded-lg shadow-md w-[600px] h-auto" alt="" />
         <p className="font-medium text-xl mt-4">Web App / <span className=" text-gray-400">ReactJS</span></p>
         </article>
         
         <article>
-        <img src="/aprobatus.png" className="rounded-lg" alt="" />
-        <p className="font-medium text-xl mt-4">Web App / <span className=" text-gray-400">ReactJS</span></p>
+        <img src="/aprobatus.png" className="rounded-lg shadow-md w-[600px] h-auto" alt="" />
+        <p className="font-medium text-xl mt-4">Education Web / <span className=" text-gray-400">Wordpress</span></p>
         </article>
-        <img src="/laliga.png" alt="" />
-        <img src="/laliga.png" alt="" />
+        <article>
+        <img src="/aprobatus.png" className="rounded-lg shadow-md w-[600px] h-auto" alt="" />
+        <p className="font-medium text-xl mt-4">Education Web / <span className=" text-gray-400">Wordpress</span></p>
+        </article>
+        <article>
+        <img src="/aprobatus.png" className="rounded-lg shadow-md w-[600px] h-auto" alt="" />
+        <p className="font-medium text-xl mt-4">Education Web / <span className=" text-gray-400">Wordpress</span></p>
+        </article>
+  
       </div>
       
 
