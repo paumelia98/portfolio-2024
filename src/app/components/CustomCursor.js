5442dd65071630d6a1423a23eccd30b0e3934d2c
Pau <paumelia98@gmail.com>
2024-08-03 16:30:41 +0200

    Update portfolio

--- src/app/components/CustomCursor.js
+++ src/app/components/CustomCursor.js
@@ -1,17 +1,59 @@
 "use client";
-
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import "@/app/globals.css";
 
 const CustomCursor = () => {
+  const canvasRef = useRef(null);
+  const requestRef = useRef();
+
   useEffect(() => {
+    if (typeof window === "undefined") {
+      return; // Solo ejecutar en el cliente
+    }
+
     const cursor = document.createElement('div');
     cursor.classList.add('custom-cursor');
     document.body.appendChild(cursor);
 
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvas.style.position = 'fixed';
+    canvas.style.left = 0;
+    canvas.style.top = 0;
+    canvas.style.pointerEvents = 'none';
+    canvas.style.zIndex = '9998'; // Justo por debajo del cursor visual
+
+    let points = []; // Almacena los puntos por los que pasa el cursor
+
     const moveCursor = (e) => {
-      cursor.style.left = `${e.clientX}px`;
-      cursor.style.top = `${e.clientY}px`;
+      // Mueve el cursor centrado
+      cursor.style.left = `${e.clientX - cursor.offsetWidth / 3}px`;
+      cursor.style.top = `${e.clientY - cursor.offsetHeight / 3}px`;
+      points.push({ x: e.clientX, y: e.clientY, time: Date.now() });
+    };
+
+    const draw = () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      let currentTime = Date.now();
+      points = points.filter(p => currentTime - p.time < 150);
+
+      if (points.length) {
+        ctx.beginPath();
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = 'rgba(0, 0, 0, 0.6)'; 
+        points.forEach((point, index) => {
+          if (index > 0) {
+            let prevPoint = points[index - 1];
+            ctx.moveTo(prevPoint.x, prevPoint.y);
+            ctx.lineTo(point.x, point.y);
+          }
+        });
+        ctx.stroke();
+      }
+
+      requestRef.current = requestAnimationFrame(draw);
     };
 
     const enlargeCursor = () => {
@@ -28,6 +70,8 @@ const CustomCursor = () => {
       link.addEventListener('mouseleave', shrinkCursor);
     });
 
+    requestRef.current = requestAnimationFrame(draw);
+
     return () => {
       document.removeEventListener('mousemove', moveCursor);
       document.querySelectorAll('a').forEach(link => {
@@ -35,10 +79,11 @@ const CustomCursor = () => {
         link.removeEventListener('mouseleave', shrinkCursor);
       });
       document.body.removeChild(cursor);
+      cancelAnimationFrame(requestRef.current);
     };
   }, []);
 
-  return null;
+  return <canvas ref={canvasRef} />;
 };
 
 export default CustomCursor;
--- src/app/components/Layout/Header.jsx
+++ src/app/components/Layout/Header.jsx
@@ -6,16 +6,14 @@ export const Header = () => {
           <a className="font-inter_tight text-black text-[16px] font-medium">Pau Melià Altadill</a>
           <span className="font-inter_tight text-[12px] text-black">Frontend Developer</span>
         </div>
-        <div>
-          <img src="/graffiti.png" alt="" className=" w-36 opacity-90 lg:block hidden" />
-        </div>
+        
         <div className="flex gap-4">
           <span className="font-inter_tight text-[14px] underline">Download CV</span>
           <span className="font-inter_tight text-[14px]">Contact</span>
         </div>
       </div>
       
-      <div className="relative h-full flex justify-center pt-28 lg:pt-60">
+      <div className="relative h-full flex justify-center pt-16 lg:pt-52">
         <video
           className="absolute top-0 left-0 w-full h-[50vh] lg:h-full object-cover rounded-b-xl border-0 -z-10  lg:block"
           src="/hero-bg.mp4"
@@ -26,11 +24,15 @@ export const Header = () => {
         ></video>
 
         <div className="absolute top-0 left-0 w-full h-[50%] z-0 rounded-b-xl"></div>
-        <div className="relative z-10 flex flex-col lg:w-[900px] w-[400px] align-center">
-          <h1 className="font-unbounded text-2xl lg:text-6xl text-center font-medium masked-text">Quality Frontend Development</h1>
-          <h2 className="font-inter_tight text-base lg:text-2xl text-center font-normal mt-4 masked-text">I specialize in developing dynamic, user-friendly websites and apps that elevate user experience, optimize performance, and drive business growth.</h2>
+        <div className=" z-10 flex flex-col lg:w-[900px] w-[400px] align-center">
+        <img src="/signature.svg" alt="" className=" lg:w-44   mx-auto mb-2 w-24" />
+
+          <h1 className="font-unbounded text-3xl lg:text-6xl text-center font-medium masked-text">Quality Frontend Development</h1>
+          <h2 className="font-inter_tight text-base lg:text-2xl text-center font-normal mt-4 masked-text mx-2">I specialize in developing dynamic, user-friendly websites and apps that elevate user experience, optimize performance, and drive business growth.</h2>
         </div>
       </div>
+
+    
     </>
   );
 };
