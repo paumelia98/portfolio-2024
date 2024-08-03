f58ae8e33d7d8c5caeef34e50f0777de583a4fb9
Pau <paumelia98@gmail.com>
2024-08-03 16:58:46 +0200

    Update portfolio

--- src/app/components/CustomCursor.js
+++ src/app/components/CustomCursor.js
@@ -4,7 +4,8 @@ import "@/app/globals.css";
 
 const CustomCursor = () => {
   const canvasRef = useRef(null);
-  const requestRef = useRef();
+  const cursorRef = useRef(null);
+  const requestRef = useRef(); // Define la referencia para manejar el requestAnimationFrame
 
   useEffect(() => {
     if (typeof window === "undefined") {
@@ -14,6 +15,7 @@ const CustomCursor = () => {
     const cursor = document.createElement('div');
     cursor.classList.add('custom-cursor');
     document.body.appendChild(cursor);
+    cursorRef.current = cursor;
 
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
@@ -23,61 +25,47 @@ const CustomCursor = () => {
     canvas.style.left = 0;
     canvas.style.top = 0;
     canvas.style.pointerEvents = 'none';
-    canvas.style.zIndex = '9998'; // Justo por debajo del cursor visual
+    canvas.style.zIndex = '9997'; // Asegúrate de que está por debajo del cursor visual
 
     let points = []; // Almacena los puntos por los que pasa el cursor
 
     const moveCursor = (e) => {
-      // Mueve el cursor centrado
-      cursor.style.left = `${e.clientX - cursor.offsetWidth / 3}px`;
-      cursor.style.top = `${e.clientY - cursor.offsetHeight / 3}px`;
+      cursor.style.left = `${e.clientX - cursor.offsetWidth / 2}px`;
+      cursor.style.top = `${e.clientY - cursor.offsetHeight / 2}px`;
       points.push({ x: e.clientX, y: e.clientY, time: Date.now() });
     };
 
     const draw = () => {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       let currentTime = Date.now();
-      points = points.filter(p => currentTime - p.time < 150);
+      points = points.filter(p => currentTime - p.time < 100);
 
-      if (points.length) {
+      if (points.length > 1) {
         ctx.beginPath();
-        ctx.lineWidth = 2;
-        ctx.strokeStyle = 'rgba(0, 0, 0, 0.6)'; 
-        points.forEach((point, index) => {
-          if (index > 0) {
-            let prevPoint = points[index - 1];
-            ctx.moveTo(prevPoint.x, prevPoint.y);
-            ctx.lineTo(point.x, point.y);
-          }
-        });
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = 'rgba(0, 0, 0, 1)'; // Hace el rastro totalmente negro
+        for (let i = 1; i < points.length - 1; i++) {
+          var xc = (points[i].x + points[i + 1].x) / 2;
+          var yc = (points[i].y + points[i + 1].y) / 2;
+          ctx.quadraticCurveTo(points[i].x, points[i].y, xc, yc);
+        }
+        ctx.quadraticCurveTo(points[points.length - 1].x, points[points.length - 1].y, points[points.length - 1].x, points[points.length - 1].y);
         ctx.stroke();
       }
 
       requestRef.current = requestAnimationFrame(draw);
     };
 
-    const enlargeCursor = () => {
-      cursor.classList.add('link-hover');
-    };
-
-    const shrinkCursor = () => {
-      cursor.classList.remove('link-hover');
-    };
-
     document.addEventListener('mousemove', moveCursor);
     document.querySelectorAll('a').forEach(link => {
-      link.addEventListener('mouseenter', enlargeCursor);
-      link.addEventListener('mouseleave', shrinkCursor);
+      link.addEventListener('mouseenter', () => cursor.classList.add('link-hover'));
+      link.addEventListener('mouseleave', () => cursor.classList.remove('link-hover'));
     });
 
     requestRef.current = requestAnimationFrame(draw);
 
     return () => {
       document.removeEventListener('mousemove', moveCursor);
-      document.querySelectorAll('a').forEach(link => {
-        link.removeEventListener('mouseenter', enlargeCursor);
-        link.removeEventListener('mouseleave', shrinkCursor);
-      });
       document.body.removeChild(cursor);
       cancelAnimationFrame(requestRef.current);
     };
