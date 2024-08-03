1f88f287b89ac097bc1ed4b9666f84cd584a3ed7
Pau <paumelia98@gmail.com>
2024-08-03 17:20:31 +0200

    Padding adjustments

--- src/app/components/Layout/Header.jsx
+++ src/app/components/Layout/Header.jsx
@@ -13,9 +13,9 @@ export const Header = () => {
         </div>
       </div>
       
-      <div className="relative h-full flex justify-center pt-16 lg:pt-40">
+      <div className="relative h-full flex justify-center pt-4 lg:pt-40">
         <video
-          className="absolute top-0 left-0 w-full h-[50vh] lg:h-full object-cover rounded-b-xl border-0 -z-10  lg:block"
+          className="absolute top-0 left-0 w-full h-[50vh] lg:h-full object-cover rounded-b-xl border-0 -z-10 lg:opacity-100"
           src="/hero-bg.mp4"
           autoPlay
           loop
@@ -28,7 +28,12 @@ export const Header = () => {
         <img src="/signature.svg" alt="" className=" lg:w-44   mx-auto mb-2 w-24" />
 
           <h1 className="font-unbounded text-3xl lg:text-6xl text-center font-medium masked-text">Quality Frontend Development</h1>
-          <h2 className="font-inter_tight text-base lg:text-2xl text-center font-normal mt-4 masked-text mx-2">I specialize in developing dynamic, user-friendly websites and apps that elevate user experience, optimize performance, and drive business growth.</h2>
+          <h2 className=" text-base lg:text-xl text-center font-normal mt-4 masked-text mx-2 text-black">I specialize in developing dynamic, user-friendly websites and apps that elevate user experience, optimize performance, and drive business growth.</h2>
+          <div className="flex gap-3 mx-auto">
+          <a href="paumelia.com" className="bg-black text-white  px-4 py-2 mx-auto mt-4 rounded  font-semibold font-inter_tight hover:scale-105">Download</a>
+          <a href="paumelia.com" className="bg-black text-white  px-4 py-2 mx-auto mt-4 rounded  font-semibold font-inter_tight hover:scale-105">Contact</a>
+
+          </div>
         </div>
       </div>
 
