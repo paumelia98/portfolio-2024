abe7c9726d4392df7ca3b83574964822f065405d
Pau <paumelia98@gmail.com>
2024-08-04 21:08:41 +0200

    flags translations

--- src/app/components/Layout/Header.jsx
+++ src/app/components/Layout/Header.jsx
@@ -23,13 +23,14 @@ export const Header = () => {
           <span className="font-inter_tight text-[12px] text-black font-light ">Frontend Developer</span>
         </header>
        
-        <div className='flex'>
-          <span className='bg-slate-300 rounded-full p-1'>ES</span>
-
-        </div>
+     
         <div className="flex gap-4 items-center">
           <span className="font-inter_tight text-[14px] underline lg:block hidden">Download CV</span>
           <span className="font-inter_tight text-[14px] lg:block hidden">Contact</span>
+          <div className='flex gap-2'>
+          <a href="/es"><img src="/flags/es-flag.svg" alt="" className='w-4 hover:scale-110' /></a>
+         <a href="/en"> <img src="/flags/uk-flag.svg" alt="" className='w-4 hover:scale-110' /></a>
+        </div>
           <button className="lg:hidden" onClick={toggleMenu}>
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M3 12h18M3 6h18M3 18h18" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
