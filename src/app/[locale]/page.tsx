2a53ad8c5b369870b45445f201a94c66f6fb4bcf
Pau <paumelia98@gmail.com>
2024-09-06 17:30:31 +0200

    about me component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,10 +2,10 @@ import { unbounded } from "@/lib/fonts/index";
 import { inter_tight } from "@/lib/fonts/index";
 import { mr_dafoe } from "@/lib/fonts/index";
 import { Hero } from '@/app/components/Layout/Hero';
-
 import { Projects } from '@/app/components/Layout/Projects';
-
 import { About } from '@/app/components/Layout/About';
+import { Footer } from '@/app/components/Layout/Footer';
+
 export default function HomePage() {
   return (
     <>
@@ -14,6 +14,7 @@ export default function HomePage() {
         <Hero />
         <Projects/>
         <About/>
+        <Footer/>
       </main>
     </>
   );
---
+++ src/app/components/Layout/Footer.jsx
@@ -0,0 +1,16 @@
+export const Footer = () => {
+
+  
+
+  return (
+    <footer className="text-center flex flex-col pt-16">
+         <a className="font-inter_tight text-black text-[16px] font-medium ">Pau Melià Altadill</a>
+         <span className="font-inter_tight text-[12px] text-black font-light ">paumelia98@gmail.com</span>
+         <span className="font-inter_tight text-[12px] text-black font-light ">© 2024 Pau Melià.</span>
+   
+
+    </footer>
+    
+)
+
+};
