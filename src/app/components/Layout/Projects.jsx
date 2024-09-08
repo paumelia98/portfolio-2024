2ec3701e7a372fa8d8a7fa656f1775ddd5df017d
Pau <paumelia98@gmail.com>
2024-09-08 15:18:00 +0200

    href updates

--- src/app/components/Layout/Projects.jsx
+++ src/app/components/Layout/Projects.jsx
@@ -1,8 +1,37 @@
+"use client";
+
 import { Project } from "@/app/components/Layout/Project";
 import { useTranslations } from 'next-intl';
+import { useLayoutEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 export const Projects = () => {
     const t = useTranslations('Projects');
+    const projectsRef = useRef([]);
+
+    useLayoutEffect(() => {
+        if (typeof window !== "undefined") {  // Asegurarte de que estás en el cliente
+            projectsRef.current.forEach((el, index) => {
+                gsap.fromTo(el,
+                    { opacity: 0, y: 50 },
+                    {
+                        opacity: 1,
+                        y: 0,
+                        duration: 1,
+                        delay: index * 0.2,
+                        scrollTrigger: {
+                            trigger: el,
+                            start: "top 80%",
+                            end: "bottom 20%",
+                            toggleActions: "play none none reverse",
+                        }
+                    });
+            });
+        }
+    }, []);
 
     const projects = [
         {
@@ -57,28 +86,31 @@ export const Projects = () => {
             description: t('description_portfolio2022')
         }
     ];
+
     return (
-        <section className="p-2 lg:px-8 lg:mt-12 mt-6 mx-4 " id="projects">
+        <section className="p-2 lg:px-8 lg:mt-12 mt-6 mx-4" id="projects">
             <div>
-                <h2 className="text-3xl font-medium ">{t('title')}</h2>
-               
-                <p className="font-extralight mt-3  mb-16">
+                <h2 className="text-3xl font-medium">{t('title')}</h2>
+                <p className="font-extralight mt-3 mb-16">
                     {t('description')}
                 </p>
             </div>
             <div className="grid md:grid-cols-2 xl:grid-cols-2 xl:gap-x-10 gap-x-10 lg:gap-y-20 gap-y-10 grid-rows-2">
-
                 {projects.map((project, index) => (
-                    <Project
+                    <div
+                        ref={(el) => projectsRef.current[index] = el}
                         key={index}
-                        title={project.title}
-                        subtitle={project.subtitle}
-                        stack={project.stack}
-                        imageUrl={project.imageUrl}
-                        videoUrl={project.videoUrl}
-                        link={project.link}
-                        description={project.description}
-                    />
+                    >
+                        <Project
+                            title={project.title}
+                            subtitle={project.subtitle}
+                            stack={project.stack}
+                            imageUrl={project.imageUrl}
+                            videoUrl={project.videoUrl}
+                            link={project.link}
+                            description={project.description}
+                        />
+                    </div>
                 ))}
             </div>
         </section>
